c61f28ac1c7baf4ba2edc04cbc0f807100b32bee
Maciej Zbierowski <maix19bep@gmail.com>
2020-09-15 12:28:56 +0200

    branch2_newroutes: some commit

--- DB/Connection.js
+++ DB/Connection.js
@@ -5,11 +5,19 @@ const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 const URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@todoscluster.qtuwh.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
-    await mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    console.log('db connected');
+    try {
+        await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('db connected');
+    } catch(err) {
+        if (!DB_NAME || !DB_PASSWORD || !DB_USERNAME) {
+            console.error('No environmental db data provided');
+            return;
+        }
+        console.error(err);
+    }
 };
 
 module.exports = connectDB;
