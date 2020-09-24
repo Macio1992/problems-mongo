b5d04ae292766eb5bc686bb3141bd6581bc17761
Maciej Zbierowski <maix19bep@gmail.com>
2020-09-24 19:39:11 +0200

    branch3_subcategories: next fixes

--- Helpers/Categories.js
+++ Helpers/Categories.js
@@ -1,3 +1,5 @@
+const { getRandomDigit, generateRandomString } = require('./Utils');
+
 class Categories {
     constructor(categories, subcategoriesAmount = 20) {
         this.categories = categories;
@@ -16,32 +18,17 @@ class Categories {
 
     getRandomSubCategory() {
         const categoriesIds = this.categories.map(category => category._id);
-        const randomIndex = this.getRandomDigit(0, categoriesIds.length);
+        const randomIndex = getRandomDigit(0, categoriesIds.length);
         const randomCategoryID = categoriesIds[randomIndex];
         const isRootCategory = false;
         const CategoryParentId = randomCategoryID;
-        const CategoryName = this.generateRandomString(20);
+        const CategoryName = generateRandomString(20);
 
         return {
             CategoryName,
             isRootCategory,
             CategoryParentId,
         }
-    }
-
-    getRandomDigit(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-    generateRandomString(length) {
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-
-        return result;
     }
 }
 
---
+++ Helpers/Utils.js
@@ -0,0 +1,17 @@
+const getRandomDigit = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const generateRandomString = length => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+module.exports = {
+    getRandomDigit,
+    generateRandomString
+}
